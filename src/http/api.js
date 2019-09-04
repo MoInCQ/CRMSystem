f9a6e9c9c3a6021f4bc771ec2664fc8e1a49edfa
lilionro <diamonds55@163.com>
2019-09-04 21:17:27 +0800

    登陆注册

---
+++ src/http/api.js
@@ -0,0 +1,12 @@
+const rootUrl = "http://192.168.100.151:8080";
+
+const loginUrl = rootUrl + "/crms/auth/login";
+
+const registerUrl = rootUrl + "/crms/auth/register";
+
+
+export default {
+  rootUrl,
+  loginUrl,
+  registerUrl,
+};
